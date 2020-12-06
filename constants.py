5039fe04f38b2bd89c9fee9ef6e7e2828ad96441
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-12-06 20:49:54 +0200

    initial commit

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'GyLrSIcjdSit0jv9F7I4Bh1uG'
+CONSUMER_SECRET = 'svx7lyyLOBqk8ODt9sQsYyxwa4VZP5OLC7zS0TyRkkSovj8vc2'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCES_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
