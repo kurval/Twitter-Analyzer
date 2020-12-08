b7d4ff0fd3ae6eded7b70e83ceb154b4d91f16a9
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-12-08 18:43:07 +0200

    refactorin and fixed empty search

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import requests
 from twitter_utils import get_request_token, get_oauth_verifier_url, get_access_token
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.secret_key = '1234'
 
 Database.initialise(database="learning", host="localhost", user="postgres", password="filsu90")
@@ -14,23 +15,19 @@ def load_user():
     if 'screen_name' in session:
         g.user = User.load_data(session['screen_name'])
 
-@app.route("/")
-def homepage():
-    return render_template('home.html')
-
-@app.route("/login/twitter")
-def twitter_login():
-    if 'screen_name' in session:
-        return redirect(url_for('profile'))
-    request_token = get_request_token()
-    session['request_token'] = request_token
-
-    return redirect(get_oauth_verifier_url(request_token))
-
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('homepage'))
+
+@app.route("/login/twitter")
+def twitter_login():
+    if 'screen_name' in session:
+        return redirect(url_for('search'))
+    request_token = get_request_token()
+    session['request_token'] = request_token
+
+    return redirect(get_oauth_verifier_url(request_token))
 
 @app.route("/auth/twitter")
 def twitter_auth():
@@ -44,15 +41,21 @@ def twitter_auth():
     user.save_to_db()
 
     session['screen_name'] = user.screen_name
-    return redirect(url_for('profile'))
+    return redirect(url_for('search'))
 
-@app.route("/profile")
-def profile():
-    return render_template('profile.html', user=g.user)
+@app.route("/")
+def homepage():
+    return render_template('login.html')
 
-@app.route('/search')
+@app.route("/search")
 def search():
+    return render_template('search.html', user=g.user)
+
+@app.route('/results')
+def results():
     query = request.args.get('q')
+    if not query:
+        return redirect(url_for('search'))
     tweets = g.user.twitter_request(f'https://api.twitter.com/1.1/search/tweets.json?q={query}')
     tweet_list = [{'tweet' : tweet['text'], 'label' : 'neutral'} for tweet in tweets['statuses']]
 
@@ -61,6 +64,7 @@ def search():
         json_response = response.json()
         tweet['label'] = json_response['label']
 
-    return render_template('search.html', tw_list=tweet_list)
+    return render_template('result.html', tw_list=tweet_list)
 
-app.run(port=4995)
+if __name__ == '__main__':
+    app.run(port=4995, debug=True)
