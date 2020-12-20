78386d7a12eb340a2e500eae6fdd71be433adbef
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-12-20 20:14:13 +0200

    adding settings

---
+++ twa_app/settings.py
@@ -0,0 +1,10 @@
+CONSUMER_KEY = 'GyLrSIcjdSit0jv9F7I4Bh1uG'
+CONSUMER_SECRET = 'svx7lyyLOBqk8ODt9sQsYyxwa4VZP5OLC7zS0TyRkkSovj8vc2'
+BEARER = 'AAAAAAAAAAAAAAAAAAAAAImZKQEAAAAAi0%2BeM95qFF24mmS%2F%2BCjD0Fb5W%2BU%3D5ucaTG3Aer6Vk5DAGG5wjvlJcahC1or8wisb56onMPcNizvQBM'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCES_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+SEARCH_URL = 'https://api.twitter.com/1.1/search/tweets.json'
+
+DB_KEY = 'filsu90'
