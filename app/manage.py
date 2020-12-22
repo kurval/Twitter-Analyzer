eee92170b20bf4e13b64b92f694d20123a5f16ca
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-12-22 13:28:23 +0200

    .

--- app/manage.py
+++ app/manage.py
@@ -1,16 +1,9 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-import os
-import sys
 
 from app import app, db
-
-if len(sys.argv) == 3:
-    MIGRATION_DIR = None
-else:
-    MIGRATION_DIR = os.path.join('app', 'migrations')
     
-migrate = Migrate(app, db, directory=MIGRATION_DIR)
+migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
--- migrations/versions/0c173ed5c908_.py
+++ migrations/versions/0c173ed5c908_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 6f39a5024934
+Revision ID: 0c173ed5c908
 Revises: 
-Create Date: 2020-12-22 13:21:22.184654
+Create Date: 2020-12-22 13:27:59.446611
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6f39a5024934'
+revision = '0c173ed5c908'
 down_revision = None
 branch_labels = None
 depends_on = None
